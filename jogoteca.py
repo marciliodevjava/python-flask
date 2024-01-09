d88e2da0efdcbc260a3aa4e8ca1796470a27a759
marcsilva <marciliodevjava@gmail.com>
2024-01-09 19:56:52 -0300

    Update jogoteca

--- jogoteca.py
+++ jogoteca.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 @app.route('/inicio')
 def ola():
-    lista_jogos = ['Tetris', 'Skytim', 'Crash']
+    jogo_um = Jogo('Tetris', 'Puzzle', 'Ataru')
+    jogo_dois = Jogo('God Of War', 'Rack in Slash', 'PS2')
+    jogo_tres = Jogo('Mortal Kombat', 'Luta', 'PS2')
+
+    lista_jogos = [jogo_um, jogo_dois, jogo_tres]
+
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
 
 
