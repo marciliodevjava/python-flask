e89d700e26ae0acd6da390e405f9b52447774c40
marcsilva <marciliodevjava@gmail.com>
2024-01-09 20:31:02 -0300

    Update jogoteca

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,15 +10,33 @@ class Jogo:
         self.console = console
 
 
+jogo_um = Jogo('Tetris', 'Puzzle', 'Ataru')
+jogo_dois = Jogo('God Of War', 'Rack in Slash', 'PS2')
+jogo_tres = Jogo('Mortal Kombat', 'Luta', 'PS2')
+
+lista_jogos = [jogo_um, jogo_dois, jogo_tres]
+
+
 @app.route('/inicio')
-def ola():
-    jogo_um = Jogo('Tetris', 'Puzzle', 'Ataru')
-    jogo_dois = Jogo('God Of War', 'Rack in Slash', 'PS2')
-    jogo_tres = Jogo('Mortal Kombat', 'Luta', 'PS2')
-
-    lista_jogos = [jogo_um, jogo_dois, jogo_tres]
-
+def inicio():
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo jogo')
+
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    lista_jogos.append(jogo)
+
+    return render_template('lista.html', titulo='Jogo', jogos=lista_jogos)
 
 
 app.run()
